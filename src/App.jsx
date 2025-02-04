79e1c23a0ece4e46e33f4373d18b1359aaf649c4
Alina Noor <AN10_23@hotmail.com>
2025-02-04 14:31:17 -0500

    Add file structure and clean vite template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { app } from "./firebase";
-import { useEffect } from "react";
+import { app } from './firebase/firebase';
+import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    console.log("Firebase App Initialized:", app);
+    console.log('Firebase App Initialized:', app);
   }, []);
 
-  return <h1 className=" bg-red-500">Check the console for Firebase connection status!</h1>;
+  return (
+    <h1 className=" bg-red-500">
+      Check the console for Firebase connection status!
+    </h1>
+  );
 }
 
 export default App;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,16 @@
 // Import the necessary Firebase modules
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 // Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET, 
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -18,7 +18,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Analytics (Only if running in the browser)
 let analytics;
-if (typeof window !== "undefined") {
+if (typeof window !== 'undefined') {
   analytics = getAnalytics(app);
 }
 
