fc8bc03a5e6b873fc6d7f620ab8e71c3c292a836
Howard Huang <howard.huang3+git@gmail.com>
2025-02-16 14:06:45 -0500

    Update index.css to new design palette

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -21,16 +21,16 @@ function Profile() {
               />
             </Card>
             {/* Profile Name  */}
-            <Card className="profile__greeting w-[15.5rem] h-[12.5rem] bg-zinc-100  rounded-[1.5rem]">
-              <CardContent className="p-4 h-full flex flex-col justify-between">
-                <p className="profile__opener font-serif text-xl font-semibold pt-[0.5rem]">
+            <Card className="profile__greeting w-[15.5rem] h-[12.5rem] rounded-[1.5rem]">
+              <CardContent className="px-4 h-full flex flex-col justify-between">
+                <p className="profile__opener font-serif text-xl font-semibold pt-[1rem]">
                   Hello 👋🏻 I’m
                 </p>
                 <div className="profile__titlebox">
                   <h2 className="profile__name text-4xl font-semibold font-serif">
                     Dianne S
                   </h2>
-                  <h3 className="profile__role text-xl font-semibold text-neutral-600 font-serif pt-[0.5rem]">
+                  <h3 className="profile__role text-xl font-semibold font-serif pt-[0.5rem]">
                     Software Developer
                   </h3>
                 </div>
@@ -38,65 +38,41 @@ function Profile() {
             </Card>
           </div>
           {/* Soft Skills  */}
-          <Card className="profile__soft-skills w-full bg-zinc-100 mt-[1rem] rounded-[1.5rem] h-[13.75rem]">
+          <Card className="profile__soft-skills w-full mt-[1rem] rounded-[1.5rem] h-[13.75rem]">
             <CardHeader>
               <CardTitle className="profile__card-title text-[1.5rem]">
                 Soft Skills
               </CardTitle>
             </CardHeader>
             <CardContent className="flex flex-row flex-wrap gap-[0.375rem] px-0 justify-center">
-              <Badge
-                className="profile__skill-badge bg-white rounded-[500px] mt-[0.5rem] h-[2rem] py-[0.25rem] w-[7.5rem] flex justify-center"
-                variant="secondary"
-              >
+              <Badge className="profile__skill-badge rounded-[500px] mt-[0.5rem] h-[2rem] w-[7.5rem] flex justify-center">
                 Communicative
               </Badge>
-              <Badge
-                className="profile__skill-badge bg-white rounded-[500px] mt-[0.5rem] h-[2rem] py-[0.25rem] w-[7.5rem] flex justify-center"
-                variant="secondary"
-              >
+              <Badge className="profile__skill-badge rounded-[500px] mt-[0.5rem] h-[2rem] w-[7.5rem] flex justify-center">
                 Analytical
               </Badge>
-              <Badge
-                className="profile__skill-badge bg-white rounded-[500px] mt-[0.5rem] h-[2rem] py-[0.25rem] w-[7.5rem] flex justify-center"
-                variant="secondary"
-              >
+              <Badge className="profile__skill-badge rounded-[500px] mt-[0.5rem] h-[2rem] w-[7.5rem] flex justify-center">
                 Collaborative
               </Badge>
-              <Badge
-                className="profile__skill-badge bg-white rounded-[500px] mt-[0.5rem] h-[2rem] py-[0.25rem] w-[7.5rem] flex justify-center"
-                variant="secondary"
-              >
+              <Badge className="profile__skill-badge rounded-[500px] mt-[0.5rem] h-[2rem] w-[7.5rem] flex justify-center">
                 Fast Learner
               </Badge>
-              <Badge
-                className="profile__skill-badge bg-white rounded-[500px] mt-[0.5rem] h-[2rem] py-[0.25rem] w-[7.5rem] flex justify-center"
-                variant="secondary"
-              >
+              <Badge className="profile__skill-badge rounded-[500px] mt-[0.5rem] h-[2rem] w-[7.5rem] flex justify-center">
                 Adaptable
               </Badge>
-              <Badge
-                className="profile__skill-badge bg-white rounded-[500px] mt-[0.5rem] h-[2rem] py-[0.25rem] w-[7.5rem] flex justify-center"
-                variant="secondary"
-              >
+              <Badge className="profile__skill-badge rounded-[500px] mt-[0.5rem] h-[2rem] w-[7.5rem] flex justify-center">
                 Critical Thinker
               </Badge>
-              <Badge
-                className="profile__skill-badge bg-white rounded-[500px] mt-[0.5rem] h-[2rem] py-[0.25rem] w-[7.5rem] flex justify-center"
-                variant="secondary"
-              >
+              <Badge className="profile__skill-badge rounded-[500px] mt-[0.5rem] h-[2rem] w-[7.5rem] flex justify-center">
                 Accountable
               </Badge>
-              <Badge
-                className="profile__skill-badge bg-white rounded-[500px] mt-[0.5rem] h-[2rem] py-[0.25rem] w-[7.5rem] flex justify-center"
-                variant="secondary"
-              >
+              <Badge className="profile__skill-badge rounded-[500px] mt-[0.5rem] h-[2rem] w-[7.5rem] flex justify-center">
                 Attentative
               </Badge>
             </CardContent>
           </Card>
           {/* Hard Skills  */}
-          <Card className="profile__hard-skills w-full bg-zinc-100 mt-[1rem] rounded-[1.5rem] h-[13.75rem]">
+          <Card className="profile__hard-skills w-full   mt-[1rem] rounded-[1.5rem] h-[13.75rem]">
             <CardHeader>
               <CardTitle className="profile__card-title text-[1.5rem]">
                 Hard Skills
@@ -107,24 +83,24 @@ function Profile() {
                 <p className="profile__skill-name text-xs w-[15%] font-semibold">
                   C++
                 </p>
-                <div className="profile__skill-slider w-[85%] bg-green-400 h-[0.75rem] border self-center"></div>
+                <div className="profile__skill-slider w-[85%] bg-amber-400 h-[0.75rem] rounded-[3px] border border-b-2 border-r-2 border-card-foreground self-center"></div>
               </div>
               <div className="profile__skill-bar flex flex-row">
                 <p className="profile__skill-name text-xs w-[15%] font-semibold">
                   Java
                 </p>
-                <div className="profile__skill-slider w-[70%] bg-green-400 h-[0.75rem] border self-center"></div>
+                <div className="profile__skill-slider w-[70%] bg-amber-400 h-[0.75rem] rounded-[3px] border border-b-2 border-r-2 border-card-foreground self-center"></div>
               </div>
               <div className="profile__skill-bar flex flex-row">
                 <p className="profile__skill-name text-xs w-[15%] font-semibold">
                   HTML
                 </p>
-                <div className="profile__skill-slider w-[25%] bg-green-400 h-[0.75rem] border self-center"></div>
+                <div className="profile__skill-slider w-[25%] bg-amber-400 h-[0.75rem] rounded-[3px] border border-b-2 border-r-2 border-card-foreground self-center"></div>
               </div>
             </CardContent>
           </Card>
           {/* Tools Card  */}
-          <Card className="profile__tools w-full bg-zinc-100 mt-[1rem] pb-[1.5rem] rounded-[1.5rem] h-[13.75rem]">
+          <Card className="profile__tools w-full mt-[1rem] pb-[1.5rem] rounded-[1.5rem] h-[13.75rem]">
             <CardHeader>
               <CardTitle className="profile__card-title text-[1.5rem]">
                 Tools
@@ -132,11 +108,14 @@ function Profile() {
             </CardHeader>
             <div className="profile__tools-bar flex flex-row justify-evenly">
               <div className="profile__tools-left flex flex-col items-center -mt-[1.438rem]">
-                <Badge className="profile__tools-badge bg-black text-white rounded-[500px] relative top-3">
+                <Badge
+                  className="profile__tools-badge text-white rounded-[500px] relative top-3"
+                  variant="secondary"
+                >
                   Code
                 </Badge>
-                <Card className="profile__code bg-white w-[13.5rem] h-[6.5rem] rounded-[1.25rem] flex justify-center items-center">
-                  <CardContent className="flex flex-row gap-[1rem] flex-wrap p-0">
+                <Card className="profile__code bg-primary border-card-foreground w-[13.5rem] h-[6.5rem] rounded-[1.25rem] flex justify-center items-center border-b-4">
+                  <CardContent className="flex flex-row gap-[0.75rem] flex-wrap py-0">
                     <img
                       src="/src/assets/icons/logo_figma.svg"
                       className="profile__icon"
@@ -165,11 +144,14 @@ function Profile() {
                 </Card>
               </div>
               <div className="profile__tools-right flex flex-col items-center -mt-[1.438rem]">
-                <Badge className="profile__tools-badge bg-black text-white rounded-[500px] relative top-3">
+                <Badge
+                  className="profile__tools-badge text-white rounded-[500px] relative top-3"
+                  variant="secondary"
+                >
                   Design
                 </Badge>
-                <Card className="profile__design bg-white w-[13.5rem] h-[6.5rem] rounded-[1.25rem] flex justify-center items-center">
-                  <CardContent className="flex flex-row gap-[1rem] flex-wrap p-0">
+                <Card className="profile__design bg-primary border-card-foreground w-[13.5rem] h-[6.5rem] rounded-[1.25rem] flex justify-center items-center border-b-4">
+                  <CardContent className="flex flex-row gap-[0.75rem] flex-wrap py-0">
                     <img
                       src="/src/assets/icons/logo_figma.svg"
                       className="profile__icon"
@@ -200,7 +182,7 @@ function Profile() {
             </div>
           </Card>
           {/* Certifications Card  */}
-          <Card className="profile__tools w-full bg-zinc-100 mt-[1rem] pb-[1.5rem] rounded-[1.5rem] h-[13.75rem]">
+          <Card className="profile__tools w-full   mt-[1rem] pb-[1.5rem] rounded-[1.5rem] h-[13.75rem]">
             <CardHeader>
               <CardTitle className="profile__card-title text-[1.5rem]">
                 Certifications
@@ -208,11 +190,14 @@ function Profile() {
             </CardHeader>
             <div className="profile__certifications-bar flex flex-row justify-evenly">
               <div className="profile__certifications-left flex flex-col items-center -mt-[1.438rem]">
-                <Badge className="profile__certifications-badge bg-black text-white rounded-[500px] relative top-3">
+                <Badge
+                  className="profile__certifications-badge text-white rounded-[500px] relative top-3"
+                  variant="secondary"
+                >
                   Code
                 </Badge>
-                <Card className="profile__code bg-white w-[13.5rem] h-[6.5rem] rounded-[1.25rem] flex justify-center items-center">
-                  <CardContent className="flex flex-row gap-[1rem] flex-wrap p-0">
+                <Card className="profile__code bg-primary border-card-foreground w-[13.5rem] h-[6.5rem] rounded-[1.25rem] flex justify-center items-center border-b-4">
+                  <CardContent className="flex flex-row gap-[0.75rem] flex-wrap py-0">
                     <img
                       src="/src/assets/icons/logo_figma.svg"
                       className="profile__icon"
@@ -241,11 +226,14 @@ function Profile() {
                 </Card>
               </div>
               <div className="profile__certifications-right flex flex-col items-center -mt-[1.438rem]">
-                <Badge className="profile__certifications-badge bg-black text-white rounded-[500px] relative top-3">
+                <Badge
+                  className="profile__certifications-badge text-white rounded-[500px] relative top-3"
+                  variant="secondary"
+                >
                   Design
                 </Badge>
-                <Card className="profile__design bg-white w-[13.5rem] h-[6.5rem] rounded-[1.25rem] flex justify-center items-center">
-                  <CardContent className="flex flex-row gap-[1rem] flex-wrap p-0">
+                <Card className="profile__design bg-primary border-card-foreground w-[13.5rem] h-[6.5rem] rounded-[1.25rem] flex justify-center items-center border-b-4">
+                  <CardContent className="flex flex-row gap-[0.75rem] flex-wrap py-0">
                     <img
                       src="/src/assets/icons/logo_figma.svg"
                       className="profile__icon"
@@ -276,9 +264,9 @@ function Profile() {
             </div>
           </Card>
           {/* Contact Card  */}
-          <Card className="profile__contact bg-amber-400 mt-[1rem] rounded-[1.5rem]">
+          <div className="profile__contact rounded-3xl border-2 border-sky-600 text-card-foreground shadow-md bg-primary mt-[1rem]">
             <CardContent>
-              <h2 className="profile__subtitle font-serif font-semibold text-white mt-[0.5rem] text-[1.5rem]">
+              <h2 className="profile__subtitle font-serif font-semibold mt-[0.5rem] text-[1.5rem]">
                 Let´s chat!
               </h2>
               <div className="profile__contact-bar flex flex-row mt-[0.5rem] items-center">
@@ -318,11 +306,11 @@ function Profile() {
                 </p>
               </div>
             </CardContent>
-          </Card>
+          </div>
         </div>
         <div className="profile__right w-[50rem] h-[1000px]">
           {/* Create Snapshot Card  */}
-          <Card className="profile__creator w-[50rem] bg-zinc-100 rounded-[1.5rem] flex flex-col items-center">
+          <Card className="profile__creator w-[50rem]   rounded-[1.5rem] flex flex-col items-center">
             <CardHeader className="profile__creator-header font-serif font-semibold text-[2.25rem]">
               Capture Your Skills in a Snapshot
             </CardHeader>
@@ -336,14 +324,14 @@ function Profile() {
               </p>
             </CardContent>
             <CardFooter>
-              <button className="profile__creator-button bg-blue-600 h-[2.5rem] w-[14.5rem] text-[1.125rem] font-semibold rounded-[0.625rem] text-white">
+              <button className="profile__creator-button bg-blue-600 h-[2.5rem] w-[14.5rem] text-[1.125rem] font-semibold rounded-[0.625rem] text-white shadow">
                 Create my Snapshot
               </button>
             </CardFooter>
           </Card>
 
           {/* What I'm Working On Card  */}
-          <Card className="profile__current w-[50rem] bg-zinc-100 rounded-[1.5rem] px-[1rem] mt-[1rem]">
+          <Card className="profile__current w-[50rem]   rounded-[1.5rem] px-[1rem] mt-[1rem]">
             <CardHeader className="profile__current-header font-serif font-semibold text-[2.25rem]">
               What I’m currently working on...
             </CardHeader>
@@ -365,7 +353,7 @@ function Profile() {
                   designer, marketing, printers, and colleagues as necessary
                 </li>
               </ul>
-              <Badge className="profile__current-badge rounded-[500px] mt-6">
+              <Badge className="profile__current-badge rounded-[500px] mt-6 border-none shadow-none">
                 Badge
               </Badge>
             </CardContent>
--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
-      'rounded-xl border bg-card text-card-foreground shadow',
+      'rounded-3xl border-2 border-amber-400 bg-card text-card-foreground shadow-md',
       className
     )}
     {...props}
