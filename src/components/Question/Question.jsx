3e67199495b548d012d8ba21e2f4373febf0d8a5
Fayo08 <oyendefavour12@gmail.com>
2025-02-19 00:21:22 -0700

    OF-retrieved ui/cardForm lost from previous commit

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -9,7 +9,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
+} from '@/components/ui/cardForm';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
