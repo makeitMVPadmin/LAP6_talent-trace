db1c33991fd8eef5d549f0591d2645f6f938b29b
Alina Noor <AN10_23@hotmail.com>
2025-02-20 19:36:05 -0500

    AN-Create a SkillsMenuCard component which uses the SelectSkills component

---
+++ src/components/SkillsMenu/SkillsMenu.jsx
@@ -0,0 +1,11 @@
+//import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from '../ui/card';
+import SelectSkills from '../SelectSkills/SelectSkills';
+
+function SkillsMenuCard() {
+  return (
+    <>
+      <SelectSkills />
+    </>
+  );
+}
+export default SkillsMenuCard;
