0b45d014973706fdb955b7e859d49a802aa02478
Alina Noor <AN10_23@hotmail.com>
2025-02-24 16:59:03 -0500

    AN-create progress bar component

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,7 +1,17 @@
-//import React from "react";
-
-function ProgressBar() {
-  return <></>;
+import PropTypes from 'prop-types';
+function ProgressBar({ isProgressing }) {
+  return (
+    <div className="w-[19.313rem] h-[0.438rem] justify-start items-center gap-[0.563rem] inline-flex">
+      <div className="w-[9.375rem] h-[0.438rem] bg-customProgressYellow rounded-[1.25rem]"></div>
+      <div
+        className={`w-[9.375rem] h-[0.438rem] ${isProgressing ? 'bg-customProgressYellow' : 'bg-customProgressGray'} rounded-[1.25rem]`}
+      ></div>
+    </div>
+  );
 }
+
+ProgressBar.PropTypes = {
+  isProgressing: PropTypes.bool.isRequired,
+};
 
 export default ProgressBar;
