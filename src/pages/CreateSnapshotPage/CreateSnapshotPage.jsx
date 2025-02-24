487c57fe93b03e02138022f3959cb053b95ecdd9
Alina Noor <AN10_23@hotmail.com>
2025-02-24 00:16:17 -0500

    AN-wrap child components with the SelectedSkillsProvider

--- src/pages/CreateSnapshotPage/CreateSnapshotPage.jsx
+++ src/pages/CreateSnapshotPage/CreateSnapshotPage.jsx
@@ -1,11 +1,14 @@
 import SkillsMenuCard from '@/components/SkillsMenu/SkillsMenu';
+import { SelectedSkillsProvider } from '@/context/SelectedSkillsContext';
 //import { Question } from '@/components/Question/Question';
 
 const CreateSnapshotPage = () => {
   return (
     <div className="flex flex-row m-32 justify-center">
-      <SkillsMenuCard />
-      {/*<Question />*/}
+      <SelectedSkillsProvider>
+        <SkillsMenuCard />
+        {/*<Question />*/}
+      </SelectedSkillsProvider>
     </div>
   );
 };
