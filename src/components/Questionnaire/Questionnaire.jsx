701d95ec2de9de743ad6b5401b8b47b036fa7f54
Alina Noor <AN10_23@hotmail.com>
2025-02-25 19:37:20 -0500

    AN-map over the selected skills for the associated question

--- src/components/Questionnaire/Questionnaire.jsx
+++ src/components/Questionnaire/Questionnaire.jsx
@@ -1,6 +1,20 @@
-//import { Question } from "../Question/Question"
+import { Question } from '../Question/Question';
+import { SelectedSkillsContext } from '@/context/SelectedSkillsContext';
+import { useContext } from 'react';
 
 function Questionnaire() {
-  return <></>;
+  const { selectedSkills } = useContext(SelectedSkillsContext);
+
+  return (
+    <div>
+      {selectedSkills.map((skill, index) => {
+        <Question
+          key={index}
+          skillId={skill.id}
+          categoryId={skill.categoryId}
+        />;
+      })}
+    </div>
+  );
 }
 export default Questionnaire;
