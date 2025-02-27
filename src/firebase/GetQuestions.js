983aab8033a2f8447e23f78fde9710edd8ffe5ab
Alina Noor <AN10_23@hotmail.com>
2025-02-27 13:23:43 -0500

    AN-fetch all questions using promise.all

--- src/firebase/GetQuestions.js
+++ src/firebase/GetQuestions.js
@@ -1,16 +1,73 @@
 import { db } from './firebase';
-import {
-  doc,
-  getDoc,
-  //collection,
-  //query,
-  //where,
-  //getDocs,
-} from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 
 // Function to get questions based on skillId
-export const getQuestions = async (skillIds, categoryId) => {
+export const getQuestions = async (skillIds, categoryIds) => {
   try {
+    const promises = skillIds.map((skillId, index) => {
+      return fetchQuestion(skillId, categoryIds[index]); //fetches each question, categoryId is provided to fetch the name of the category
+    });
+
+    const questions = await Promise.all(promises);
+    return questions.filter(Boolean);
+  } catch (error) {
+    console.error('Error fetching questions:', error);
+    return [];
+  }
+};
+
+const fetchQuestion = async (skillId, categoryId) => {
+  try {
+    // Fetch the category name for the given categoryId
+    const categoryRef = doc(db, 'categories', categoryId);
+    const categorySnap = await getDoc(categoryRef);
+
+    if (!categorySnap.exists()) {
+      console.log('No category found');
+      return [];
+    }
+
+    const categoryName = categorySnap.data().name;
+
+    // Fetch the question related to the skillId
+
+    const skillRef = doc(db, 'skills', skillId);
+    const skillSnap = await getDoc(skillRef);
+
+    if (skillSnap.exists()) {
+      const skillData = skillSnap.data();
+      const { questionId, skillName } = skillData;
+
+      //check if questionId exists and retrive the question
+      if (questionId) {
+        const questionRef = doc(db, 'questions', questionId);
+        const questionSnap = await getDoc(questionRef);
+
+        if (questionSnap.exists()) {
+          const questionData = questionSnap.data();
+          const question = questionData.question;
+
+          //return question along with the skillName Id, categorName, id
+          return {
+            question,
+            skillName,
+            skillId,
+            categoryName,
+            categoryId,
+          };
+        }
+      }
+    }
+
+    return null;
+  } catch (error) {
+    console.error('Error fetching question', error);
+    return null;
+  }
+};
+
+/*
+
     // Fetch the category name for the given categoryId
     const categoryRef = doc(db, 'categories', categoryId);
     const categorySnap = await getDoc(categoryRef);
@@ -62,3 +119,5 @@ export const getQuestions = async (skillIds, categoryId) => {
     return [];
   }
 };
+
+*/
